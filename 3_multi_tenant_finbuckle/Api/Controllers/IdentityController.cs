2761645bddb2ea45b2e3a697b945670412d85c8d
chinaq <aaa6263@sina.com>
2020-09-29 13:18:37 +0800

    init project 3

---
+++ 3_multi_tenant_finbuckle/Api/Controllers/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Api.Tenant;
+
+namespace Api.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    // [CustomAuthorize(AuthenticationSchemes = "Bearer")]
+    public class IdentityController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
